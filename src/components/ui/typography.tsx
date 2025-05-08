afc3048a4eaf9b2cddf5391e4cb7acdb4a90c6be
Zoubair <dev.zoubair@gmail.com>
2025-05-08 20:41:00 +0100

    feat(tailwind): configure typography, color scheme, and theme variations with switch case.

--- src/components/ui/typography.tsx
+++ src/components/ui/typography.tsx
@@ -36,15 +36,83 @@ const Typography = ({
 
   switch (variants) {
     case "display":
-      variantStyles = "text-red-500";
+      variantStyles = "text-8xl";
       break;
+    case "h1":
+      variantStyles = "text-7xl";
+      break;
+    case "h2":
+      variantStyles = "text-6xl";
+      break;
+    case "h3": //Default
+      variantStyles = "text-5xl";
+      break;
+    case "h4":
+      variantStyles = "text-4xl";
+      break;
+    case "h5":
+      variantStyles = "text-3xl";
+      break;
+    case "lead":
+      variantStyles = "text-2xl";
+      break;
+    case "lead":
+      variantStyles = "text-2xl";
+      break;
+    case "body-lg":
+      variantStyles = "text-lg";
+      break;
+    case "body-base":
+      variantStyles = "text-base";
+      break;
+    case "body-sm":
+      variantStyles = "text-sm";
+      break;
+    case "xs":
+      variantStyles = "text-xs";
+      break;
+    case "2xs":
+      variantStyles = "text-2xs";
+      break;
+    case "3xs":
+      variantStyles = "text-3xs";
+      break;
+    case "4xs":
+      variantStyles = "text-4xs";
+      break;
+    default:
+      break;
+  }
 
+  switch (themes) {
+    case "black": //Default
+      colorStyles = "text-gray";
+      break;
+    case "gray":
+      colorStyles = "text-gray-400";
+      break;
+    case "white":
+      colorStyles = "text-white";
+      break;
+    case "primary":
+      colorStyles = "text-primary";
+      break;
+    case "secondary":
+      colorStyles = "text-secondary";
+      break;
     default:
       break;
   }
 
   return (
-    <Component className={cn(variantStyles, weight == "medium" && "font-medium", className)}>
+    <Component
+      className={cn(
+        variantStyles,
+        colorStyles,
+        weight == "medium" && "font-medium",
+        className
+      )}
+    >
       {children}
     </Component>
   );
